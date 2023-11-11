fa3d9fda547517835e89f0c81263499ceabfb787
Sayantan <web.dev.sayantan@gmail.com>
2023-11-11 22:38:56 +0530

    initial commit for remote app one

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const {
+  share,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  name: "remote-app-two",
+
+  exposes: {
+    "./Module": "./src/app/app.module.ts",
+  },
+  shared: share({
+    "@angular/core": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/common/http": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+    "@angular/router": {
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    },
+  }),
+});
